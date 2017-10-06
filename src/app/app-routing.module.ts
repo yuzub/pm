1b39b17e427c274816c98bf21e59c0f5cf505f13
Yuriy Zubaha <yuzub@ua.fm>
2017-10-06 03:16:47 +0300

    + customer - template-driven form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CustomerComponent } from './customers/customer.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'customer', component: CustomerComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent }
     ]),
