4f4ea36cd44bf7d49d5aad889d10e18c944ef648
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-10-09 18:40:40 +0300

    + some validation

--- src/app/customers/customer-rf.component.ts
+++ src/app/customers/customer-rf.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  // FormControl,
+  FormBuilder, Validators, AbstractControl, ValidatorFn
+} from '@angular/forms';
 
 import { Customer } from './customer';
+
+// custom validator function
+/* function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
+  if (c.value != undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+    return { 'range': true };
+  }
+  return null;
+} */
+
+//customr validator function with parameters
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true };
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'pm-signup-rf',
@@ -12,14 +34,30 @@ export class CustomerRfComponent implements OnInit {
   //Data model below
   customer: Customer = new Customer();
 
+  constructor(private fb: FormBuilder) { }
+
   ngOnInit(): void {
-    //This structure is the form model, and tracks the form value and state.
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
+    //3 var. of syntax can be used with FormBuilder
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+        confirmEmail: ['', Validators.required],
+      }),
+      phone: '',
+      notification: 'email',
+      rating: ['', ratingRange(1, 5)],
+      sendCatalog: true
     });
+
+    //This structure is the form model, and tracks the form value and state.
+    /*     this.customerForm = new FormGroup({
+          firstName: new FormControl(),
+          lastName: new FormControl(),
+          email: new FormControl(),
+          sendCatalog: new FormControl(true)
+        }); */
   }
 
   populateTestAllData(): void {
@@ -42,5 +80,17 @@ export class CustomerRfComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+
+  //adjusting validation rules at runtime
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone'); // reference to the phone FormControl
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
